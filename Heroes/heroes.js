5473879eb9b35d5691604ae28aafc2e8d32d4d86
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-07-29 17:28:42 +0530

    Create heroes.js

---
+++ Heroes/heroes.js
@@ -0,0 +1,5 @@
+//Grab a reference to the header tag in the HTML file
+const header = document.querySelector('header');
+
+//Grab a reference to the section tag in the HTML file
+const section = document.querySelector('section');
