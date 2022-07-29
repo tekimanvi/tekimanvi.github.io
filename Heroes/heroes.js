cb82ca467604c14067add26556949672fb691951
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-07-29 17:43:04 +0530

    Update heroes.js

--- Heroes/heroes.js
+++ Heroes/heroes.js
@@ -4,4 +4,18 @@ const header = document.querySelector('header');
 //Grab a reference to the section tag in the HTML file
 const section = document.querySelector('section');
 
+//Where the Json file is
+
 let requestURL = 'https://tekimanvi.github.io/Heroes/superheroes.json';
+
+//Generating a XHR Request 
+let request = new XMLHttpRequest();
+
+// Opens the json - two parameters - type of request and the URL to make the request to
+request.open('GET', requestURL);
+
+//  here we are setting the responseType to JSON, so that XHR knows that the server will be returning JSON
+request.responseType = 'json';
+
+//Sending Request 
+request.send();
