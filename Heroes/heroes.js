9cebf9489d805e13e0f4084f5e0af2d22faf0f06
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-07-29 17:31:11 +0530

    Update heroes.js

--- Heroes/heroes.js
+++ Heroes/heroes.js
@@ -4,4 +4,4 @@ const header = document.querySelector('header');
 //Grab a reference to the section tag in the HTML file
 const section = document.querySelector('section');
 
-let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let requestURL = 'https://tekimanvi.github.io/Heroes/superheroes.json';
