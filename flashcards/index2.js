29131686a87ade615d50ae1e7d683a3d43bed919
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-08-11 01:33:47 +0530

    Create index2.js

---
+++ flashcards/index2.js
@@ -0,0 +1,66 @@
+let myflashcards = [
+    {
+        question: "What color is the sky?",
+        answer: "Blue"
+    },
+    {
+        question: "What color is grass?",
+        answer: "Green"
+    },
+    {
+        question: "What color stripes does a zebra have?",
+        answer: "Black and White "
+    }]
+
+let i = 0;
+let ques = document.getElementById('q');
+let ans = document.getElementById('a');
+let display = document.getElementById('Q');
+let next = document.getElementById('N');
+
+display.addEventListener("mouseover",function(){
+    // display.textContent="changed";
+    ques.innerHTML = a;
+  })
+
+let a='';
+let b='';
+
+function update(){
+     a=myflashcards[i].question;
+     b=myflashcards[i].answer;
+}
+
+while (i < myflashcards.length){
+     a=myflashcards[i].question;
+     b=myflashcards[i].answer;
+  
+  display.addEventListener("mouseover",function(){
+    update();
+    ques.innerHTML = a;
+  })
+  
+   ques.addEventListener("mouseover",function(){
+    update();
+    ans.innerHTML = b;
+    i++;
+    console.log(i);
+    
+  })
+ 
+ next.addEventListener("mouseover",function() {
+    // i++;
+    // console.log(i); 
+    // a=myflashcards[i].question;
+     update();     
+     console.log(a);
+     ques.innerHTML = a;
+        ans.innerHTML = '';
+          
+    });
+    
+    break;
+
+ 
+ }
+   
