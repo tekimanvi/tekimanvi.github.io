1fb0e6e95f7bac99ec18752414e81bcce0395425
tekimanvi <107298271+tekimanvi@users.noreply.github.com>
2022-08-10 09:01:54 +0530

    Create index.js

---
+++ flashcards/index.js
@@ -0,0 +1,22 @@
+let myflashcards = [
+    {
+        question:"What color is the sky?",
+        answer:"Blue"
+    },
+    {
+        question:"What color is grass?",
+        answer:"Green"
+    },
+    {
+        question:"What color stripes does a zebra have?",
+        answer:"Black and White "
+    }]
+    
+let i = 0;
+
+while (i < myflashcards.length)  {
+     console.log(myflashcards[i].question);
+  
+     
+   ++i
+}
